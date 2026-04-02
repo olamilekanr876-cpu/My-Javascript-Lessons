f8a5f1c8b912c80494016747c23d61d85561fc33
Ridwan Raji <olamilekanr876@gmail.com>
2026-04-02 12:04:00 +0100

    javascript lesson started

---
+++ assignment.js
@@ -0,0 +1,30 @@
+const  myName='Raji Ridwan Olalekan'
+console.log(myName)
+console.log(typeof(myName))
+
+const  datatypes=['boolean','numbers', 'string','bigints' ,'object' ]
+ 
+const boolean=[true,false]
+console.log(boolean)
+
+const numbers=[2,20,200]
+console.log(numbers)
+
+const string= 'strings refer to texts in Javascript'
+console.log(string)
+
+const bigints= [5895255555587955789, 359878564685n]
+console.log(bigints)
+
+const dog={
+    name: 'jaguar ',
+    age: '5years',
+    breed:'german shepherd',
+}
+console.log(dog)
+
+
+const fruits=['mango','apple','orange',]
+
+console.log(fruits[1])
+
---
+++ script.js
@@ -0,0 +1,123 @@
+ const heading = document.getElementById('heading')
+         const colorBtn = document.getElementById('colorBtn')
+
+         colorBtn.addEventListener('click', ()=>{
+            heading.style.color = 'green'
+            heading.innerText = 'Dataslid Javascript Tutorial' 
+         })
+
+         const nyName = "This is Ayo0la's laptop"
+         console.log(nyName)
+
+        //  alert('Hello Ridwan')
+        
+
+         console.log('My name is Ridwan')
+
+         let fruits = 'orange';
+         fruits = 'pawpaw'
+         console.log(fruits)
+         fruits = 'mango'
+
+         console.log(fruits)
+
+
+         const a = 5
+         const b = 10
+         const z = a+b
+         console.log(z)
+         console.log(a-b)
+         console.log(a+b)
+         console.log(a*b)
+         console.log(a/b)
+         console.log(b%3 + 6 - a)
+
+         const meta = 0
+
+         const Meta = 1
+
+         const _meta = 3
+
+         const name= 10
+
+         const stringExample =  'MTH101'
+         const myGender = '0.5'
+
+         console.log(typeof(myGender))
+
+         const bigIntExample = 2023232525585225252525622n
+         console.log(typeof(bigIntExample))
+
+         const check = 7 > 5
+         console.log(check)
+
+         const hasPVC =  false
+         console.log(typeof(hasPVC))
+
+
+         const myName = ''
+          console.log(typeof(myName))
+
+
+         let myAge;
+         console.log(typeof(myAge))
+         myAge = 10
+           console.log(typeof(myAge))
+
+
+
+           const Ridwan = {
+             location: 'Ota-Efun',
+             gender: 'male',
+             age:  10,
+             isSingle : false,
+
+           }
+
+
+           const car = {
+             make: 'toyota',
+             model : 'camry',
+             year: 2016,
+             color: 'blue',
+             transmission: 'manual'
+           }
+         //We can acess elements of an object via dot notation or bracket notation
+
+           console.log(Ridwan.location)
+           console.log(Ridwan.gender)
+           console.log(Ridwan['age'])
+
+
+
+           const vegetables = ['ewedu','cucumber','carrot','tete']
+
+           console.log(vegetables)
+
+           console.log(vegetables[2])
+
+
+           const vehicles = ['mazda','nissan', 'honda', 'bmw', 'volvo','lexus']
+
+           console.log(vehicles[2])
+
+
+
+// Assignment 
+// create a new javascript file and attach it to html 
+ // create a  variable and store your name inside it
+ //print your name in the console. 
+ //check the data type of your variable.
+
+ // list the Javascript data types you know with examples
+
+ // create an object of a dog with name, age, breed
+
+ //log the breed to the console.
+
+
+ // create an array of fruits. log the second item in the array to the console.
+
+        
+
+
